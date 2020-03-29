e50386559e704c6e2f0e87dc522d477eb5b374a6
Adrian Armijo <zidelblack@hotmail.com>
2020-03-29 16:08:42 -0500

    Avance a clase 24 - Composer

---
+++ jobs.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+
+use App\Models\{Job, Project, Printable};
+
+$job1 = new Job('PHP Developer', 'This is an awesome job!!!');
+$job1->months = 16;
+
+$job2 = new Job('Python Developer', 'This is an awesome job!!!');
+$job2->months = 24;
+
+$job3 = new Job('Devops', 'This is an awesome job!!!');
+$job3->months = 32;
+
+$project1 = new Project('Project 1','Description 1');
+
+$jobs = [
+    $job1,
+    $job2,
+    $job3
+  ];
+
+$projects = [
+    $project1
+];
+  
+function printElement(Printable $job) {
+    if($job->visible == false) {
+      return;
+    }
+  
+    echo '<li class="work-position">';
+    echo '<h5>' . $job->getTitle() . '</h5>';
+    echo '<p>' . $job->getDescription() . '</p>';
+    echo '<p>' . $job->getDurationAsString() . '</p>';
+    echo '<strong>Achievements:</strong>';
+    echo '<ul>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+    echo '</ul>';
+    echo '</li>';
+  }
